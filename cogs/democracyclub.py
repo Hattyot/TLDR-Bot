c2485e738380217e1fd4c31410a9234f0be5bb74
William (elf gold) <maberwilliam@gmail.com>
2021-10-11 09:13:32 +0100

    split upc ode

--- cogs/democracyclub.py
+++ cogs/democracyclub.py
@@ -18,12 +18,7 @@ import datetime
 import config
 import matplotlib.pyplot as plt
 import os
-import itertools, sys
-spinner = itertools.cycle(['-', '/', '|', '\\'])
-def spin(name =""):
-    sys.stdout.write(name+" "+next(spinner))   # write the next character
-    sys.stdout.flush()                # flush stdout buffer (actual character display)
-    sys.stdout.write('\b'*(len(name)+2))            # erase the last written char
+from until.spin import spin
 
 
 # >>democracy_club
