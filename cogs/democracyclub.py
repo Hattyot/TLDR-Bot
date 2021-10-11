daadb1c5380172ee99a4c7deb570d06bd3cf5552
William (elf gold) <maberwilliam@gmail.com>
2021-10-11 09:02:26 +0100

    push beforwe  push as reqest

--- cogs/democracyclub.py
+++ cogs/democracyclub.py
@@ -14,57 +14,181 @@ import discord
 import config
 from discord.ext import tasks
 import re
-import datetime 
-import config 
+import datetime
+import config
 import matplotlib.pyplot as plt
+import os
+import itertools, sys
+spinner = itertools.cycle(['-', '/', '|', '\\'])
+def spin(name =""):
+    sys.stdout.write(name+" "+next(spinner))   # write the next character
+    sys.stdout.flush()                # flush stdout buffer (actual character display)
+    sys.stdout.write('\b'*(len(name)+2))            # erase the last written char
 
-##>>democracy_club
+
+# >>democracy_club
 ENDPOINT_DEMOCRACY_CLUB = "https://candidates.democracyclub.org.uk/api/next/"
-ENDPOINT_WHEREDOIVOTE= "https://wheredoivote.co.uk/api/beta/"
+ENDPOINT_WHEREDOIVOTE = "https://wheredoivote.co.uk/api/beta/"
 p = re.compile(r'([0-9]*)-([0-9]*)-([0-9]*)T([0-9]*):([0-9]*):([0-9]*)\+([0-9]*)', re.IGNORECASE)
 
-def TimeStop(thing,path_a,path_b):
-        time = p.search(thing[path_a][path_b])
-        then = datetime.datetime(int(time[1]),int(time[2]),int(time[3]),int(time[4]),int(time[5]),int(time[6]))
-        now  = datetime.datetime.now()
-        then_duration = datetime.timedelta(**config.timediff)
-        duration = now - then
-        return not duration<then_duration
 
+def timeStop(thing, path_a, path_b):
+    time = p.search(thing[path_a][path_b])
+    then = datetime.datetime(int(time[1]), int(time[2]), int(time[3]), int(time[4]), int(time[5]), int(time[6]))
+    now = datetime.datetime.now()
+    then_duration = datetime.timedelta(**config.timediff)
+    duration = now - then
+    return not duration < then_duration
 
 
 class DemocracyClub(Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.slug =[]
-        self.facebook_adverts_id =[]
+        self.slug = []
+        self.facebook_adverts_id = []
+        #
+        self.person_name= {}
+        self.party = {}
+        self.funding_entity = []
+        self.region_distribution =[]
 
     @Cog.listener()
     async def on_ready(self):
-        await self.facebook_ad()
+        await self.facebook_ad_post(await self.facebook_ad_scan())
         self.democracy_club.start()
         self.facebook_ad.start()
     
+    async def facebook_ad_scan(self,block_list = []):
+        url = ENDPOINT_DEMOCRACY_CLUB + "facebook_adverts/"
+        loop = True
+        results_ = []
+        while loop:
+            spin("downloading for facebook_ads")
+            r = requests.get(url)
+            data = r.json()
+            results_ = results_ + data["results"]
+            for result in data["results"]:
+                if timeStop(result, "ad_json", "ad_creation_time"):
+                    loop = False
+                    break
+                if result["ad_id"] in block_list:
+                    loop = False
+                    break
+            if data["next"] is None:
+                break
+            else:
+                url = data["next"]
+        for results in results_:
+            spin("scaning results of facebook_ads")
+            # for self.candidacie["party"]["ec_id"] not in  data_person["candidacies"]:
+            #     if candidacie["party"]["ec_id"] in self.party.keys():
+            #         self.party[self.candidacie["party"]["ec_id"]] = self.candidacie["party"]["name"]
+            if results["person"]["id"] not in  self.person_name.keys(): 
+                r = requests.get(results["person"]["url"])
+                data_person = r.json()
+                self.person_name[data_person["id"]] =  data_person["honorific_prefix"]+" "+ data_person["name"]
+            if "funding_entity" in results["ad_json"].keys():
+                if results["ad_json"]["funding_entity"] not in self.region_distribution:
+                    self.funding_entity.append(results["ad_json"]["funding_entity"])
+            if "region_distribution" in results["ad_json"].keys():
+                if results["ad_json"]["region_distribution"] not in self.region_distribution:
+                    self.region_distribution.append(results["ad_json"]["region_distribution"])
+        return results_
+
     @command(
         help='Show someone you polling station based on their home address.',
         usage="polls",
         examples=["polls"],
         cls=commands.Command,
     )
-    async def polling_station(self, ctx: Context,postcode):
+    async def get_facebookads_by_most_recent_candidate_of_party(self,ctx:Context,regx):
+        pass
+
+    @command(
+        help='Show someone you polling station based on their home address.',
+        usage="polls",
+        examples=["polls"],
+        cls=commands.Command,
+    )
+    async def get_facebookads_by_page_name(self,ctx:Context,regx):
+        pass
+
+    @command(
+        help='Show someone you polling station based on their home address.',
+        usage="polls",
+        examples=["polls"],
+        cls=commands.Command,
+    )
+    async def get_facebookads_by_funding_entity(self,ctx:Context,regx):
+        pass
+
+    @command(
+        help='Show someone you polling station based on their home address.',
+        usage="polls",
+        examples=["polls"],
+        cls=commands.Command,
+    )
+    async def get_facebookads_by_person_name(self,ctx:Context,regx):
+        pass
+
+    @command(
+        help='Show someone you polling station based on their home address.',
+        usage="polls",
+        examples=["polls"],
+        cls=commands.Command,
+    )
+    async def list_partys(self,ctx:Context,regx):
+        pass
+
+    @command(
+        help='Show someone you polling station based on their home address.',
+        usage="polls",
+        examples=["polls"],
+        cls=commands.Command,
+    )
+    async def list_person(self,ctx:Context,regx):
+        pass
+
+    @command(
+        help='Show someone you polling station based on their home address.',
+        usage="polls",
+        examples=["polls"],
+        cls=commands.Command,
+    )
+    async def list_page_name(self,ctx:Context,regx):
+        pass
+
+    @command(
+        help='Show someone you polling station based on their home address.',
+        usage="polls",
+        examples=["polls"],
+        cls=commands.Command,
+    )
+    async def list_funding_entity(self,ctx:Context,regx):
+        pass
+
+
+
+    @command(
+        help='Show someone you polling station based on their home address.',
+        usage="polls",
+        examples=["polls"],
+        cls=commands.Command,
+    )
+    async def polling_station(self, ctx: Context, postcode):
         url = ENDPOINT_WHEREDOIVOTE + "postcode/{postcode}.json".format(postcode=postcode)
         r = requests.get(url)
         data = r.json()
-        polling_station_known = data ["polling_station_known"]
-        electoral_services_contacts_phone_numbers = data ["council"]["electoral_services_contacts"]["phone_numbers"]
-        electoral_services_contacts_email = data ["council"]["electoral_services_contacts"]["email"]
-        electoral_services_contacts_address = data ["council"]["electoral_services_contacts"]["address"]
-        electoral_services_contacts_postcode = data ["council"]["electoral_services_contacts"]["postcode"]
-        electoral_services_contacts_website = data ["council"]["electoral_services_contacts"]["website"]
+        polling_station_known = data["polling_station_known"]
+        electoral_services_contacts_phone_numbers = data["council"]["electoral_services_contacts"]["phone_numbers"]
+        electoral_services_contacts_email = data["council"]["electoral_services_contacts"]["email"]
+        electoral_services_contacts_address = data["council"]["electoral_services_contacts"]["address"]
+        electoral_services_contacts_postcode = data["council"]["electoral_services_contacts"]["postcode"]
+        electoral_services_contacts_website = data["council"]["electoral_services_contacts"]["website"]
         #electoral_services_contacts_identifiers = data ["council"]["electoral_services_contacts"]["identifiers"]
         #nation = data ["council"]["electoral_services_contacts"]["nation"]
-        council_id = data ["council"]["council_id"]
-        embed=discord.Embed()
+        council_id = data["council"]["council_id"]
+        embed = discord.Embed()
         embed.add_field(name="polling_station_known", value=polling_station_known, inline=False)
         embed.add_field(name="electoral services contacts phone_numbers", value=electoral_services_contacts_phone_numbers, inline=False)
         embed.add_field(name="electoral services contacts email", value=electoral_services_contacts_email, inline=False)
@@ -76,26 +200,25 @@ class DemocracyClub(Cog):
         embed.add_field(name="council id", value=council_id, inline=False)
         await ctx.send(embed=embed)
 
-
     @tasks.loop(seconds=10.0)
     async def democracy_club(self):
         # pull data from democracyclub
         results_feed = feedparser.parse(r'https://candidates.democracyclub.org.uk/results/all.atom')
         # Check the Channel just i case i dont know what as been sent before
-        given_name ="democracyclub"
+        given_name = "democracyclub"
         channel = discord.utils.get(self.bot.get_all_channels(), name=given_name)
-        msgs = await channel.history(limit=len(results_feed.entries)*2).flatten()
+        msgs = await channel.history(limit=len(results_feed.entries) * 2).flatten()
         for msg in msgs:
             if msg.embeds != []:
                 if msg.embeds[0].footer.text not in self.slug:
                     self.slug.append(msg.embeds[0].footer.text)
-        for entries_i in range(1,len(results_feed.entries)-1):
+        for entries_i in range(1, len(results_feed.entries) - 1):
             entries = results_feed.entries[entries_i]
-                
+
             if entries["election_slug"] in self.slug:
                 continue
             self.slug.append(entries["election_slug"])
-            embed= discord.Embed(title=entries["title"], url=entries["information_source"])
+            embed = discord.Embed(title=entries["title"], url=entries["information_source"])
             if "image_url" in entries.keys():
                 img_url = entries["image_url"]
                 img_url = img_url.replace("https://candidates.democracyclub.org.uk/", "")
@@ -106,90 +229,95 @@ class DemocracyClub(Cog):
             embed.add_field(name="post name", value=entries["post_name"], inline=False)
             embed.add_field(name="election name", value=entries["election_name"], inline=False)
             embed.add_field(name="election date", value=entries["election_date"], inline=False)
-            embed.set_footer(text=entries["election_slug"]) 
+            embed.set_footer(text=entries["election_slug"])
             self.slug.append(entries["election_slug"])
             await channel.send(embed=embed)
+            
 
+        
 
     @tasks.loop(seconds=10.0)
     async def facebook_ad(self):
-        given_name ="facebook-adverts-poitcal"
+        results_ = await self.facebook_ad_scan(self.facebook_adverts_id)
+        await self.facebook_ad_post(results_)
+    
+    async def facebook_ad_post(self,results_):
+        given_name = "facebook-adverts-poitcal"
         channel = discord.utils.get(self.bot.get_all_channels(), name=given_name)
-        url = ENDPOINT_DEMOCRACY_CLUB + "facebook_adverts/"
         msgs = await channel.history(limit=200).flatten()
-
         for msg in msgs:
             if msg.embeds != []:
-                if msg.embeds[0].footer.text not in self.slug:
+                if msg.embeds[0].footer.text not in self.facebook_adverts_id:
                     self.facebook_adverts_id.append(msg.embeds[0].footer.text)
-
-        results_ = []
-        print(self.facebook_adverts_id)
-        loop = True
-        while loop:
-                
-            r = requests.get(url)
-            data = r.json()
-            results_ = results_ + data["results"]
-            print(url)
-            for result in data["results"]:
-                if TimeStop(result,"ad_json","ad_creation_time"):
-                   loop = False
-                   break
-                if result["ad_id"] in self.facebook_adverts_id:
-                    loop = False
-                    break
-            if data["next"] is None:
-                break
-            else:
-                url = data["next"]
         results_.reverse()
         for result in results_:
             if result["ad_id"] in self.facebook_adverts_id:
-                continue
-            embed= discord.Embed(title=result["ad_json"]["page_name"], url=result["associated_url"])
+                break
+            embed = discord.Embed(title=result["ad_json"]["page_name"], url=result["associated_url"])
             embed.set_thumbnail(url=result["image"])
             embed.add_field(name="page_name", value=result["ad_json"]["page_name"], inline=False)
             if "funding_entity" in result["ad_json"].keys():
                 embed.add_field(name="funding_entity", value=result["ad_json"]["funding_entity"], inline=False)
-            
+
             if "ad_creative_body" in result["ad_json"].keys():
                 if len(result["ad_json"]["ad_creative_body"]) < 1024:
                     embed.add_field(name="ad_creative_body", value=result["ad_json"]["ad_creative_body"], inline=False)
                 else:
-                    embed.add_field(name="ad_creative_body", value=result["ad_json"]["ad_creative_body"][0:1000] +"...", inline=False)
+                    embed.add_field(name="ad_creative_body", value=result["ad_json"]["ad_creative_body"][0:1000] + "...", inline=False)
             embed.add_field(name="ad_creation_time", value=result["ad_json"]["ad_creation_time"], inline=False)
-            embed.add_field(name="currency", value=result["ad_json"]["currency"], inline=False)
-            embed.add_field(name="spend", value=str(result["ad_json"]["spend"]["lower_bound"])+">"+str(result["ad_json"]["spend"]["upper_bound"]), inline=False)
+            if "ad_delivery_stop_time" in result["ad_json"].keys():
+                embed.add_field(name="ad_delivery_stop_time", value=result["ad_json"]["ad_delivery_stop_time"], inline=False)
+            if "ad_delivery_start_time" in result["ad_json"].keys():
+                embed.add_field(name="ad_delivery_start_time", value=result["ad_json"]["ad_delivery_start_time"], inline=False)
+            embed.add_field(name="spend", value="between inclusively: " + str(result["ad_json"]["spend"]["lower_bound"]) + " and " + str(result["ad_json"]["spend"]["upper_bound"] + " " + result["ad_json"]["currency"]), inline=False)
+            if "impressions" in result["ad_json"].keys():
+                embed.add_field(name="impressions", value="between inclusively: " + str(result["ad_json"]["impressions"]["lower_bound"]) + " and " + str(result["ad_json"]["impressions"]["upper_bound"]), inline=False)
             embed.add_field(name="person", value=result["person"]["name"], inline=False)
-            embed.set_footer(text=result["ad_id"]) 
+            embed.set_footer(text=result["ad_id"])
+            #embed.set_image(url="attachment://"+"grath_facebook_ad" + str(result["ad_id"]) + ".png")
+
             self.facebook_adverts_id.append(result["ad_id"])
-            ff = {
-                "18-24":0,
-                "25-34":1,
-                "35-44":2,
-                "45-54":3,
-                "55-64":4,
-                "65+":5,
+            ageRageIndex = {
+                "13-17": 0,
+                "18-24": 1,
+                "25-34": 2,
+                "35-44": 3,
+                "45-54": 4,
+                "55-64": 5,
+                "65+": 6,
             }
-            men_means = [0, 0, 0, 0, 0, 0]
-            women_means = [0, 0, 0, 0, 0, 0]
-            unknown_means = [0, 0, 0, 0, 0, 0]
+            men_means = [None,None, None, None, None, None, None]
+            women_means = [None,None, None, None, None, None, None]
+            unknown_means = [None,None, None, None, None, None, None]
+            region_percentage = []
+            region_lab = []
             for i in result["ad_json"]["demographic_distribution"]:
                 if i["gender"] == "male":
-                    men_means[ff[i["age"]]] = float(i["percentage"])
+                    men_means[ageRageIndex[i["age"]]] = float(i["percentage"])
                 elif i["gender"] == "female":
-                    women_means[ff[i["age"]]] = float(i["percentage"])
+                    women_means[ageRageIndex[i["age"]]] = float(i["percentage"])
                 elif i["gender"] == "unknown":
-                    unknown_means[ff[i["age"]]] = float(i["percentage"])
+                    unknown_means[ageRageIndex[i["age"]]] = float(i["percentage"])
+            for i in result["ad_json"]["region_distribution"]:
+                region_lab.append(i["region"])
+                region_percentage.append(i["percentage"])
             width = 0.35
-            labels = ['18-24','25-34','35-44','45-54','55-64','65+']
-            x = np.arange(len(labels))
-            plt.plot(labels, men_means, label ="men")
-            plt.plot(labels, women_means, label ="women")
-            plt.plot(labels, unknown_means, label ="unknown")
-            plt.show()
-            await channel.send(embed=embed)
+            labels = ["13-17",'18-24', '25-34', '35-44', '45-54', '55-64', '65+']
+            fig, (ax1, ax2) = plt.subplots(2)
+            ax1.set_title('demographic distribution')
+            ax1.plot(labels, men_means,"o", label="men")
+            ax1.plot(labels, women_means,"o", label="women")
+            ax1.plot(labels, unknown_means,"o", label="unknown")
+            ax1.legend()
+            ax2.set_title('region')
+            ax2.plot(region_lab, region_percentage,"bo", label="region")
+            plt.legend()
+            plt.savefig("grath_facebook_ad" + str(result["ad_id"]) + ".png")
+            plt.close()
+            image = discord.File("grath_facebook_ad" + str(result["ad_id"]) + ".png", filename="grath_facebook_ad" + str(result["ad_id"]) + ".png")
+            await channel.send(embed=embed, file=image)
+            os.remove("grath_facebook_ad" + str(result["ad_id"]) + ".png")
+
 
 def setup(bot):
     bot.add_cog(DemocracyClub(bot))
